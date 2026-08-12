b4019be1a5d53ae01442e74c99f04d8bddd08749
Eliott Tornassat <eliott.tornassat@epfl.ch>
2026-08-12 14:19:57 +0200

    add support for multi input for the v system

---
+++ assets/js/recettes/v/utils.js
@@ -0,0 +1,61 @@
+// original code: https://mgechev.github.io/javascript-algorithms/graphs_others_topological-sort.js.html
+function topologicalSort(graph, vertices) {
+  function topologicalSortHelper(node, visited, temp, graph, result) {
+    temp[node] = true;
+    var neighbors = vertices(graph[node]);
+    for (var i = 0; i < neighbors.length; i += 1) {
+      var n = neighbors[i];
+      if (temp[n]) {
+        throw new Error('The graph is not a DAG');
+      }
+      if (!visited[n]) {
+        topologicalSortHelper(n, visited, temp, graph, result);
+      }
+    }
+    temp[node] = false;
+    visited[node] = true;
+    result.push(node);
+  }
+  /**
+    * Topological sort algorithm of a directed acyclic graph.<br><br>
+    * Time complexity: O(|E| + |V|) where E is a number of edges
+    * and |V| is the number of nodes.
+    *
+    * @public
+    * @module graphs/others/topological-sort
+    * @param {Array} graph Adjacency list, which represents the graph.
+    * @returns {Array} Ordered vertices.
+    *
+    * @example
+    * var topsort =
+    *  require('path-to-algorithms/src/graphs/' +
+    * 'others/topological-sort').topologicalSort;
+    * var graph = {
+    *     v1: ['v2', 'v5'],
+    *     v2: [],
+    *     v3: ['v1', 'v2', 'v4', 'v5'],
+    *     v4: [],
+    *     v5: []
+    * };
+    * var vertices = topsort(graph); // ['v3', 'v4', 'v1', 'v5', 'v2']
+    */
+  var result = [];
+  var visited = [];
+  var temp = [];
+  for (var node in graph) {
+    if (!visited[node] && !temp[node]) {
+      topologicalSortHelper(node, visited, temp, graph, result);
+    }
+  }
+  return result.reverse();
+};
+
+function arrayDiff(arr1, arr2) {
+  const set1 = new Set(arr1);
+  const set2 = new Set(arr2);
+  
+  return [
+    ...arr1.filter(x => !set2.has(x)),
+    ...arr2.filter(x => !set1.has(x))
+  ];
+}
---
+++ assets/js/recettes/v/v.js
@@ -0,0 +1,300 @@
+function getElementForEachId(...ids) {
+  let array = []
+  let notFound = false
+  for (const id of ids) {
+    let element = document.getElementById(id)
+    if (element == undefined) {
+      console.warn(`Warning : ${id} could not be found. Make sure an element has that id.`)
+      notFound = true;
+
+      continue
+    }
+
+    array.push(element)
+  }
+  
+  if (notFound) {
+    return undefined
+  } else {
+    return array
+  }
+}
+
+function processElements(elements) {
+  const invalidElements = []
+  const processedElements = []
+  for (const element of elements) {
+    const baseNumberStr = element.getAttribute('v-base-value')
+    if (!baseNumberStr) {
+      invalidElements.push(element)
+      continue
+    }
+    const baseNumber = parseFloat(baseNumberStr)
+    if (baseNumber == NaN) {
+      invalidElements.push(element)
+      continue
+    }
+    const labelElement = element.querySelector('#label')
+    if (!labelElement) {
+      invalidElements.push(element)
+      continue
+    }
+
+    // We use nan to check if step is set
+    let step = parseFloat(element.getAttribute('v-step'))
+    if (step == 0.0) {
+      invalidElements.push(element)
+      continue
+    }
+
+    // We use nan to check if step is set
+    let min = parseFloat(element.getAttribute('v-min'))
+    if (min <= 0.0) {
+      invalidElements.push(element)
+      continue
+    }
+
+    const round = element.getAttribute('v-round') || "default"
+    let roundingMethod;
+    switch (round) {
+      case "default":
+        roundingMethod = Math.round
+        break
+      case "floor":
+        roundingMethod = Math.floor
+        break
+      case "ceil":
+        roundingMethod = Math.ceil
+        break
+      default:
+        invalidElements.push(element)
+        continue
+    }
+
+    processedElements.push({
+      container: element, 
+      element: labelElement,
+      baseNumber: baseNumber,
+      step,
+      min,
+      roundingMethod
+    })
+  }
+  if (invalidElements.length > 0) {
+    throw `Internal Error : V system with id ${vId} contains invalid elements : ${invalidElements}`
+  }
+
+  return processedElements
+}
+
+function formatNumberWithUnits(number, min) {
+  // round to two digits by default
+  let formattedVal = Math.round(number * 10) / 10
+  if (!Number.isNaN(min)) {
+    formattedVal = Math.max(min, formattedVal)
+  }
+  
+  return formattedVal.toString()
+}
+
+function formatNumberWithStep(number, step, min, roundFunction) {
+  let round = roundFunction(number / step)
+  if (round == 0.0) {
+    round = Math.ceil(number / step)
+  }
+  
+  let formattedVal = step * round;
+  if (!Number.isNaN(min)) {
+    formattedVal = Math.max(min, formattedVal)
+  }
+  return formattedVal.toString()
+}
+
+function gcd(a, b) {
+  a = Math.abs(a)
+  b = Math.abs(b)
+  return b === 0 ? (a || 1) : gcd(b, a % b)
+}
+
+function updateElement(element, newRatio) {
+  let newElementValue = element.baseNumber * newRatio
+
+  let formattedValue;
+  // We use nan to check if step is set
+  if (!Number.isNaN(element.step)) {
+    formattedValue = formatNumberWithStep(newElementValue, element.step, element.min, element.roundingMethod)
+  } else {
+    formattedValue = formatNumberWithUnits(newElementValue, element.min)
+  }
+  
+  if (element.element.innerText != formattedValue) {
+    element.element.innerText = formattedValue
+
+    requestAnimationFrame(() => {
+      element.container.classList.remove('value-changed')
+
+      requestAnimationFrame(() => {
+        element.container.classList.add('value-changed')
+      })
+    })
+  }
+}
+
+function registerVSystem(id, system, position, sortedSystemIds, systems) {
+  const elements = getElementForEachId(...system.elements || [])
+  if (elements == undefined) {
+    return;
+  }
+  const processedElements = processElements(elements)
+  
+  const container = document.getElementById(`${id}_input`)
+  if (!container) {
+    console.warn(`vSystem ${id} is not linked to an input`)
+    return
+  }
+
+  const input = container.querySelector('input')
+  if (!input) {
+    console.warn(`vSystem ${id} is not linked to an input`)
+    return
+  }
+  
+  const btnIncElement = container.querySelector('#btn-inc')
+  const btnDecElement = container.querySelector('#btn-dec')
+
+  let lastValidInput = system.baseValue;
+  input.value = system.baseValue;
+  system.value = system.baseValue;
+  
+  function updateValue(inputValue) {
+    let newValue = parseInt(inputValue)
+
+    if (Number.isNaN(newValue)) {
+      return false
+    }
+
+    newValue = Math.max(newValue, system.min)
+    if (Object.hasOwn(system, "max")) {
+      newValue = Math.min(newValue, system.max)
+    }
+    input.value = newValue
+    
+    lastValidInput = newValue;
+    system.value = newValue;
+
+    for (const element of processedElements) {
+      updateElement(element, newValue / system.baseValue)
+    }
+
+    // Note : Normally we would only try to update the dependents of that value,
+    // but i don't know how to do that efficiently while maintining update order.
+    // 
+    // So i'm trying to update all the values that changed sequentially, in the
+    // topologically sorted array. It's maybe underoptimized, but it should work.
+    for (let i = position + 1; i < sortedSystemIds.length; i += 1) {
+      const system = systems[sortedSystemIds[i]];
+      if (!system.dependencies) {
+        continue
+      }
+
+      const dependencyVariables = {};
+      for (const id of system.dependencies) {
+        dependencyVariables[id] = systems[id].value
+      }
+      const newValue = system.expression.evaluate(dependencyVariables);
+
+      if (system.value != newValue) {
+        system.value = newValue;
+        
+        for (const element of system.elements) {
+          updateElement(element, system.value / system.baseValue)
+        }
+      }
+    }
+
+    return true
+  }
+
+  input.addEventListener('input', () => updateValue(input.value))
+  input.addEventListener('blur', () => {
+    if (!updateValue(container.value)) {
+      container.value = lastValidInput
+      updateValue(lastValidInput)
+    }
+  })
+  // disable wheeling
+  input.addEventListener('wheel', (e) => {
+    e.preventDefault();
+  }, { passive: false });
+
+  if (btnIncElement) {
+    btnIncElement.addEventListener('click', () => {
+      updateValue(parseInt(input.value) + 1)
+    })
+  }
+  if (btnDecElement) {
+    btnDecElement.addEventListener('click', () => {
+      updateValue(parseInt(input.value) - 1)
+    })
+  }
+}
+
+function validateExpression(systemId, system, expressionStr, parser) {
+  let expression;
+  try {
+    expression = parser.parse(expressionStr)
+  } catch (e) {
+    console.error(`v system ${systemId} has an invalid expression '${system.expression}' : ${e}`)
+    return null;
+  }
+
+  const expressionVariables = new Set(expression.variables())
+  const dependencySet = new Set(system.expressionVariables)
+
+  if (!expressionVariables.isSubsetOf(dependencySet)) {
+    const unknownVariables = expression.variables().filter(x => !dependencySet.has(x))
+
+    console.error(`v system ${systemId} has unknown variables '${unknownVariables}'`)
+    return null;
+  }
+
+  return expression
+}
+
+function registerVSystems(vSystems) {
+  const sortedVSystems = topologicalSort(vSystems, (system) => system.dependents || [])
+  const parser = new exprEval.Parser()
+  
+  for (let i = 0; i < sortedVSystems.length; i += 1) {
+    const systemId = sortedVSystems[i];
+    const system = vSystems[sortedVSystems[i]];
+
+    if (system.expressionVariables) {
+      const expression = validateExpression(systemId, system, system.expression, parser)
+      if (expression === null) {
+        continue;
+      }
+      system.expression = expression;
+      
+      const elements = getElementForEachId(...system.elements || [])
+      if (elements == undefined) {
+        return;
+      }
+      const processedElements = processElements(elements)
+      system.elements = processedElements;
+
+      const dependencyVariables = {};
+      for (const id of system.dependencies) {
+        dependencyVariables[id] = vSystems[id].value
+      }
+      system.value = system.expression.evaluate(dependencyVariables);
+      system.baseValue = system.value;
+
+      for (const element of processedElements) {
+        updateElement(element, system.value / system.baseValue)
+      }
+    } else {
+      registerVSystem(systemId, system, i, sortedVSystems, vSystems)
+    }
+  }
+}
